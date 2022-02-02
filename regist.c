0fa01681bd26bb1b27b5a8c49194a7ddbb189e9e
YiDongYeol <98364404+YiDongYeol@users.noreply.github.com>
2022-02-03 08:52:54 +0900

    Update regist.c

--- regist.c
+++ regist.c
@@ -4,20 +4,21 @@
 void postRegist(Post *_Post) {
 	int cur_x = TITLE_START_X, cur_y = TITLE_START_Y;
 	if (postCount == POST_MAX) {
-		printf("ÃÖ´ë ±Û °³¼ö¸¦ ³Ñ½À´Ï´Ù.\n");
+		printf("ìµœëŒ€ ê¸€ ê°œìˆ˜ë¥¼ ë„˜ìŠµë‹ˆë‹¤.\n");
+		system("pause");
 		return;
 	}
 	postInitializing(&(_Post[postCount]));
 	_Post[postCount].curLine = 1;
 	while (1) {
 		system("cls");
-		printf(" ±Û ÀÛ¼º \n");
+		printf(" ê¸€ ìž‘ì„± \n");
 		printf("==========================================================\n");
-		printf(" Á¦¸ñ : %s\n",_Post[postCount].titleText);
+		printf(" ì œëª© : %s\n",_Post[postCount].titleText);
 		printf("==========================================================\n");
 		for (int i = 0; i < _Post[postCount].curLine; i++)
 			printf("%02d %s\n", i + 1, _Post[postCount].mainText[i]);
-		printf("\nF1 - ÀúÀå  esc - ³ª°¡±â");
+		printf("\nF1 - ì €ìž¥  esc - ë‚˜ê°€ê¸°");
 		gotoxy(cur_x, cur_y);
 		switch (input(&(_Post[postCount]), &cur_x, &cur_y)) {
 		case 1:
