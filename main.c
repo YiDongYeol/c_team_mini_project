d031e76973a1e5e3ead345d42efb89ec4a1ba83d
YiDongYeol <98364404+YiDongYeol@users.noreply.github.com>
2022-02-07 08:45:24 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -2,29 +2,59 @@
 
 void main() {
 	Post _Post[POST_MAX];
-	sample(_Post);
+	User _User[USER_MAX];
+	int loginSuccess = 0;
+	int flag = 0;
+	sample(_Post, _User);
 	while (1) {
-		
-		
-		system("cls");
-		switch (menu()) {
-		case 1:
-			postRegist(_Post);
-			break;
-		case 2:
-			postEdit(_Post);
-			break;
-		case 3:
-			postView(_Post);
-			break;
-		case 4:
-			postDelete(_Post);
-			break;
-		case 5:
-			//º¹±¸
-			break;
-		case 6:
-			return;
+		while (1) {
+			system("cls");
+			switch (loginMenu()) {
+			case 1:
+				loginSuccess = login(_User);
+				break;
+			case 2:
+				userRegist(_User);
+				break;
+			case 0:
+				return;
+			}
+			if (loginSuccess)
+				break;
+		}
+		while (1) {
+			system("cls");
+			switch (menu()) {
+			case 1:
+				postRegist(_Post);
+				break;
+			case 2:
+				postEdit(_Post);
+				break;
+			case 3:
+				postView(_Post);
+				break;
+			case 4:
+				postDelete(_Post);
+				break;
+			case 5:
+				if (currentUserType == IS_ADMIN)
+					postRecovery(_Post);
+				else
+					flag = userEdit(_User);
+				break;
+			case 6:
+				if (currentUserType == IS_ADMIN)
+					userManage(_User);
+				else
+					flag = userBlind(_User);
+				break;
+			case 0:
+				flag = 1;
+				break;
+			}
+			if (flag)
+				break;
 		}
 	}
 }
